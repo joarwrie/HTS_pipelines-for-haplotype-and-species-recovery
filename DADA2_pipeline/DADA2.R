f2e92161b01b1c295804b4bbf5f50830f6b233db
joarwrie <67008125+joarwrie@users.noreply.github.com>
2020-06-19 16:00:25 +0200

    Update DADA2.R

--- DADA2_pipeline/DADA2.R
+++ DADA2_pipeline/DADA2.R
@@ -71,7 +71,7 @@ tableau=tableau[tableau$Compte!=0,]
 tableau$Echantillon=do.call(rbind, strsplit(as.character(tableau$Sample), "_"))[,1]
 tableau$Replicat=paste(do.call(rbind, strsplit(as.character(tableau$Sample), "_"))[,2], do.call(rbind, strsplit(as.character(tableau$Sample), "_"))[,3], sep="_")
 tableau=as.data.table(tableau)
-tableau=tableau[tableau$Sample!="Undetermined",]
+tableau=tableau[tableau$Echantillon!="Undetermined",]
 
 # Identification of the maximum number of reads in a index control sample
 neg=tableau[grep("^neg[0-9]", tableau$Echantillon),]
