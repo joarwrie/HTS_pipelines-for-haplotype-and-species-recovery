ed2c30ed0d75efac6871e672522d2c5f4fbccdfb
joarwrie <67008125+joarwrie@users.noreply.github.com>
2020-09-28 22:30:06 +0200

    Update Data_treatment.R

--- VS+SWARM_pipeline/Data_treatment.R
+++ VS+SWARM_pipeline/Data_treatment.R
@@ -1,25 +1,37 @@
 library(data.table)
 
-# Filtering for index-jumping and replicates
+# Importing file
 entree=read.csv("AllSamples_finalTAB.csv", header=T, sep="\t", dec=".")
 tableau=melt(entree, variable.name="UV_Sample", value.name="Nb_reads", id.vars="X.OTU.ID")
 tableau=tableau[tableau$Nb_reads!=0,]
 tableau$Echantillon=do.call(rbind, strsplit(as.character(tableau$UV_Sample), "_"))[,1]
 tableau$Replicat=paste(do.call(rbind, strsplit(as.character(tableau$UV_Sample), "_"))[,2], do.call(rbind, strsplit(as.character(tableau$UV_Sample), "_"))[,3], sep="_")
+OTU_table=read.csv("AllSamples_OTUlist_modif.txt", header=F, sep="\t", dec=".", stringsAsFactors=F)
+colnames(OTU_table)=c("Seed", "SeqID")
+tableau=merge(tableau, OTU_table, by.x="X.OTU.ID", by.y="SeqID", all=T)
 tableau=as.data.table(tableau)
 tableau=tableau[tableau$Echantillon!="Undetermined",]
-tableau[,"Compte":=sum(Nb_reads), by=.(X.OTU.ID, Echantillon, Replicat)]
-tableau=unique(tableau[,c(1,4,5,6)])
+tableau[,"Somme":=sum(Nb_reads), by=.(Seed, Echantillon)]
+tableau=unique(tableau[,4:7])
 
 # Identification of the maximum number of reads in a index control sample
 neg=tableau[grep("^neg[0-9]", tableau$Echantillon),]
-maxIndex=max(neg$Compte)
-tab_tagJump=tableau[tableau$Compte>=maxIndex,]
+maxIndex=max(neg$Somme)
+
+# Filtering for index jump
+tab_tagJump=tableau[tableau$Somme>=2*maxIndex,]
+
+# Filtering for replicates
 tab_tagJump=tab_tagJump[tab_tagJump$Replicat!="ext1_PCR1" & tab_tagJump$Replicat!="ext1_PCR5" & tab_tagJump$Replicat!="ext2_PCR2" & tab_tagJump$Replicat!="ext2_PCR4" & tab_tagJump$Replicat!="ext3_PCR1" & tab_tagJump$Replicat!="ext3_PCR3" & tab_tagJump$Replicat!="ext4_PCR4" & tab_tagJump$Replicat!="ext4_PCR5",]
-tab_tagJump[,"RepCheck":=.N, by=.(Echantillon, X.OTU.ID)]
+tab_tagJump$Type="Ethanol"
+tab_tagJump[grep("bulk",tab_tagJump$Echantillon),]$Type="Bulk"
+tab_tagJump$Bon=TRUE
+tab_tagJump[tab_tagJump$Type=="Bulk" & (tab_tagJump$Replicat=="ext4_PCR1" | tab_tagJump$Replicat=="ext4_PCR2" |tab_tagJump$Replicat=="ext4_PCR3"),]$Bon=FALSE
+tab_tagJump[tab_tagJump$Type=="Ethanol" & (tab_tagJump$Replicat=="ext1_PCR2" | tab_tagJump$Replicat=="ext1_PCR3" |tab_tagJump$Replicat=="ext1_PCR4"),]$Bon=FALSE
+tab_tagJump=tab_tagJump[tab_tagJump$Bon==TRUE,]
+tab_tagJump[,"RepCheck":=.N, by=.(Echantillon, Seed)]
 tab_final=tab_tagJump[tab_tagJump$RepCheck>4,]
-tab_final$Sample=paste(tab_final$Echantillon, tab_final$Replicat, sep="_")
 
 # Exporting final contingency table
-tab_sortie=dcast(tab_final, X.OTU.ID~Sample, value.var="Compte", fun.aggregate=sum, fill=0)
-write.table(file="Contingency_table_VSSwarm.csv", tab_sortie, col.name=T, row.name=F, sep="\t", dec=".", quote=F)
+tab_sortie=dcast(tab_final, Seed~Echantillon, value.var="Somme", fun.aggregate=sum, fill=0)
+write.table(file="Contingency_table_OBISwarm.csv", tab_sortie, col.name=T, row.name=F, sep="\t", dec=".", quote=F)
